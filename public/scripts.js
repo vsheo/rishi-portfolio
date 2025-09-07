d865489528633aafad788254a15a11a261387a2e
vsheo <vireshsheoratan@gmail.com>
2025-09-07 22:44:43 +0200

    JS om kleur en font te veranderen

---
+++ public/scripts.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body')
+const theme = document.querySelector('.change-theme')
+const font = document.querySelector('.change-font')
+
+let oldTheme = 'palette-1'
+let oldFont = 'font-1'
+
+
+document.querySelectorAll('.change-theme input[name="palette"]').forEach(radio => {
+    radio.addEventListener('change', (e) => {
+        let newTheme = radio.id
+        body.classList.remove(oldTheme)
+        body.classList.add(newTheme)
+        oldTheme = newTheme;
+    })
+})
+
+theme.removeAttribute('hidden')
+
+document.querySelectorAll('.change-font input[name="font"]').forEach(radio => {
+    radio.addEventListener('change', () => {
+        let newFont = radio.id
+        body.classList.remove(oldFont)
+        body.classList.add(newFont)
+        oldFont = newFont;
+    })
+})
+
+font.removeAttribute('hidden')
