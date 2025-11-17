a17888f44a989e15f475c75f5ad73fd8c8d69ec2
vsheo <vireshsheoratan@gmail.com>
2025-11-17 13:19:15 +0100

    interaction button comments

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,28 +1,18 @@
-// select button and section
+// select all interaction buttons
 const allButton = document.querySelectorAll('.interaction-button');
-// console.log(allButton.item(index))
 
-// // select the secttions that belong to this button
-// const currentView = document.querySelectorAll('.grid-cell > *:nth-of-type(1)');
-// const newView = document.querySelectorAll('.grid-cell > *:nth-of-type(2)');
-// console.log(currentView)
-// console.log(newView)
-
+// listen for a click, and remember the index of that klik
 allButton.forEach((button, index) => {
 	button.addEventListener('click', (e) => {
-		// select the secttions that belong to this button
+		// +1 because CSS starts counting at 1
 		let cellNumber = index + 1
+
+		// select the children of the grid-cell that corresponds to the button that was clicked. (the children in the same container as the button)
 		const currentView = document.querySelector(`.grid-cell:nth-of-type(${cellNumber}) > *:nth-of-type(1)`);
 		const newView = document.querySelector(`.grid-cell:nth-of-type(${cellNumber}) > *:nth-of-type(2)`);
 
-		console.log(cellNumber);
+		// change the one that is displayed
 		currentView.classList.toggle('hidden');
 		newView.classList.toggle('hidden');
 	});
 });
-
-// change target
-// button.addEventListener('click', () => {
-// 	currentView.classList.toggle('hidden');
-// 	newView.classList.toggle('hidden');
-// });
